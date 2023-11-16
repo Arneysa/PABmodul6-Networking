c80e97bff1260614242d6912899719ee7ecbd24b
Neysa <arneysa02gmail.com>
2023-11-16 15:34:27 +0700

    Modul6-Networking

---
+++ components/index.js
@@ -0,0 +1,11 @@
+import Header from "./header";
+import CategoryButton from "./category-button";
+import Categories from "./categories";
+import NewsItem from "./news-item";
+
+export { Header, CategoryButton, Categories, NewsItem };
+
+// import Header from "./header";
+import Header from "./header";
+
+export {Header};
